acf58210f3dc8827447b3e5aaa8f1774584350b7
Nick Chapman <nic@amadido.co.uk>
2022-12-01 05:15:30 +0000

    day1

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,21 +1,22 @@
 module Day1 (main) where
 
 import Misc (check)
-import Par4 (Par,parse,terminated,nl,int)
+import Par4 (Par,parse,separated,terminated,nl,int)
+import Data.List as List (sort)
 
 main :: IO ()
 main = do
-  inp <- load "input/day1.input"
-  print ("day1, part1", check 5 $ part1 inp)
+  inp <- parse gram <$> readFile "input/day1.input"
+  print ("day1, part1", check 71506 $ part1 inp)
+  print ("day2, part2", check 209603 $ part2 inp)
 
-load :: FilePath -> IO Setup
-load path = parse gram <$> readFile path
-
-gram :: Par [Line]
-gram = terminated nl int
-
-type Setup = [Line]
-type Line = Int
+type Setup = [[Int]]
 
 part1 :: Setup -> Int
-part1 = length
+part1 = maximum . map sum
+
+part2 :: Setup -> Int
+part2 = sum . take 3 . reverse . List.sort . map sum
+
+gram :: Par Setup
+gram = separated nl (terminated nl int)
