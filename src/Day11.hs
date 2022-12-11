ce7f3fbe1d1693ceb33c9ba944d8358bd6bc450d
Nick Chapman <nic@amadido.co.uk>
2022-12-11 21:12:05 +0000

    combine code for parts 1 & 2 (time --> 0.5s)

--- src/Day11.hs
+++ src/Day11.hs
@@ -14,10 +14,13 @@ main = do
 
   print ("day11, part1 (sam)", check 10605 $ part1 sam)
   print ("day11, part1", check 108240 $ part1 inp)
-  print ("day11, part1 (sam)", check 2713310158 $ part2 sam)
-  print ("day11, part1 (sam)", check 25712998901 $ part2 inp)
+  print ("day11, part2 (sam)", check 2713310158 $ part2 sam)
+  print ("day11, part2", check 25712998901 $ part2 inp)
 
-----------------------------------------------------------------------
+  where
+    part1 = partX True 20
+    part2 = partX False 10000
+
 
 type Setup = [(MonkeyId,Desc)]
 data Desc = Desc [Int] Op Decide deriving Show
@@ -74,35 +77,36 @@ gram = separated nl monkey
     mid :: Par MonkeyId
     mid = char
 
-----------------------------------------------------------------------
 
-data State1 = State1
+data State = State
   { hold :: Map MonkeyId [Int]
   , count :: Map MonkeyId Int
   }
 
-instance Show State1 where show State1{count} = show count
-
-initState1 :: Setup -> State1
-initState1 q = do
+initState :: Setup -> State
+initState q = do
   let xs = [ (k,ws) | (k,Desc ws _ _) <- q ]
-  State1 { hold = Map.fromList xs, count = Map.empty }
+  State { hold = Map.fromList xs, count = Map.empty }
 
-part1 :: Setup -> Int
-part1 q = do
-  let ss = iterate round (initState1 q)
-  let s' = head (drop 20 ss)
-  let State1{count} = s'
+partX :: Bool -> Int -> Setup -> Int
+partX doDiv n q = do
+  let
+    loop :: Int -> State -> State
+    loop i s = do
+      if (i == n) then s else loop (i+1) (round s)
+  let s' = loop 0 (initState q)
+  let State{count} = s'
   let [a,b] = take 2 $ reverse (List.sort (map snd (Map.toList count)))
-  (a*b)
+  (a * b)
+
   where
-    round :: State1 -> State1
+    round :: State -> State
     round s0 = foldl (flip turn) s0 (map fst q)
 
-    turn :: MonkeyId -> State1 -> State1
+    turn :: MonkeyId -> State -> State
     turn k = loop
       where
-        loop :: State1 -> State1
+        loop :: State -> State
         loop s0 = do
           let (opt,s) = inspect k s0
           case opt of
@@ -118,130 +122,36 @@ part1 q = do
     worry :: MonkeyId -> Int -> Int
     worry k w = do
       let (op,_) = look k odm
-      apply w op `div` 3
+      apply w op `div` (if doDiv then 3 else 1)
 
     apply :: Int -> Op -> Int
     apply w = \case
       Add i -> w + i
-      Mul i -> w * i
-      Square -> w * w
+      Mul i -> reduce (w * i)
+      Square -> reduce (w * w)
+
+      where
+        reduce :: Int -> Int
+        reduce x = x `mod` m
+          where
+            m :: Int
+            m = 2*3*5*7*11*13*17*19*23
 
     decide :: MonkeyId -> Int -> MonkeyId
     decide k w = do
       let (_,Decide d k1 k2) = look k odm
       if w `mod` d == 0 then k1 else k2
 
-    inspect :: MonkeyId -> State1 -> (Maybe Int, State1)
-    inspect k s0@State1{hold,count} = do
+    inspect :: MonkeyId -> State -> (Maybe Int, State)
+    inspect k s0@State{hold,count} = do
       let ws = look k hold
       case ws of
         [] -> (Nothing, s0)
         w:ws -> do
-          let s :: State1 = s0 { count = Map.insertWith (+) k 1 count }
+          let s :: State = s0 { count = Map.insertWith (+) k 1 count }
           (Just w, s { hold = Map.insert k ws hold })
 
-    chuckTo :: MonkeyId -> Int -> State1 -> State1
-    chuckTo k w s@State1{hold} = do
+    chuckTo :: MonkeyId -> Int -> State -> State
+    chuckTo k w s@State{hold} = do
       let ws = look k hold
       s { hold = Map.insert k (ws++[w]) hold }
-
-
-----------------------------------------------------------------------
-
-data State2 = State2
-  { hold :: Map MonkeyId [Inty]
-  , count :: Map MonkeyId Int
-  }
-
-instance Show State2 where show State2{count} = show count
-
-initState2 :: Setup -> State2
-initState2 q = do
-  let xs = [ (k,map ofInt ws) | (k,Desc ws _ _) <- q ]
-  State2 { hold = Map.fromList xs, count = Map.empty }
-
-part2 :: Setup -> Int
-part2 q = do
-  let
-    loop :: Int -> State2 -> State2
-    loop i s = do
-      if (i == 10000) then s else loop (i+1) (round s)
-  let s' = loop 0 (initState2 q)
-  let State2{count} = s'
-  let [a,b] = take 2 $ reverse (List.sort (map snd (Map.toList count)))
-  (a * b)
-
-  where
-    round :: State2 -> State2
-    round s0 = foldl (flip turn) s0 (map fst q)
-
-    turn :: MonkeyId -> State2 -> State2
-    turn k = loop
-      where
-        loop :: State2 -> State2
-        loop s0 = do
-          let (opt,s) = inspect k s0
-          case opt of
-            Nothing -> s
-            Just w -> do
-              let w' = worry k w
-              let k' = decide k w'
-              loop (chuckTo k' w' s)
-
-    odm :: Map MonkeyId (Op,Decide)
-    odm = Map.fromList [ (k,(o,d)) | (k,Desc _ o d) <- q ]
-
-    worry :: MonkeyId -> Inty -> Inty
-    worry k w = do
-      let (op,_) = look k odm
-      apply w op -- `div` 3
-
-    apply :: Inty -> Op -> Inty
-    apply w = \case
-      Add i -> w `add` i
-      Mul i -> w `mul` i
-      Square -> square w
-
-    decide :: MonkeyId -> Inty -> MonkeyId
-    decide k w = do
-      let (_,Decide d k1 k2) = look k odm
-      if w `divisible` d then k1 else k2
-
-    inspect :: MonkeyId -> State2 -> (Maybe Inty, State2)
-    inspect k s0@State2{hold,count} = do
-      let ws = look k hold
-      case ws of
-        [] -> (Nothing, s0)
-        w:ws -> do
-          let s :: State2 = s0 { count = Map.insertWith (+) k 1 count }
-          (Just w, s { hold = Map.insert k ws hold })
-
-    chuckTo :: MonkeyId -> Inty -> State2 -> State2
-    chuckTo k w s@State2{hold} = do
-      let ws = look k hold
-      s { hold = Map.insert k (ws++[w]) hold }
-
-----------------------------------------------------------------------
-
-data Inty = Inty Int deriving (Show)
-
-ofInt :: Int -> Inty
-ofInt a = Inty (fromIntegral a)
-
-divisible :: Inty -> Int -> Bool
-divisible (Inty a) b = (a `mod` b) == 0
-
-add :: Inty -> Int -> Inty
-add (Inty a) b = Inty (a+b)
-
-mul :: Inty -> Int -> Inty
-mul (Inty a) b = Inty (reduce (a*b))
-
-square :: Inty -> Inty
-square (Inty a) = Inty (reduce (a*a))
-
-reduce :: Int -> Int
-reduce x = x `mod` m
-  where
-    m :: Int
-    m = 2*3*5*7*11*13*17*19*23
