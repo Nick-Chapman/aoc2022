acd1e5aeb530d8093047cc80f77e070e67f14c8c
Nick Chapman <nic@amadido.co.uk>
2022-12-18 21:02:54 +0000

    day 17 part 1, clean up

--- src/Day17.hs
+++ src/Day17.hs
@@ -7,16 +7,13 @@ import Data.Set (Set)
 
 main :: IO ()
 main = do
-  _sam <- parse gram <$> readFile "input/day17.sam"
-  _inp <- parse gram <$> readFile "input/day17.input"
-
-  --res <- part1 _sam
-  --print ("day17, part1", check 0 $ res) -- 3068
-
-  res <- part1 _inp
-  print ("day17, part1", check 0 $ res) --3106
+  sam <- parse gram <$> readFile "input/day17.sam"
+  inp <- parse gram <$> readFile "input/day17.input"
+  print ("day17, part1 (sam)", check 3068 $ part1 sam)
+  print ("day17, part1", check 3106 $ part1 inp)
 
 type Setup = [LR]
+data LR = L | R deriving Show
 
 gram :: Par Setup
 gram = do xs <- many lr; nl; pure xs
@@ -24,13 +21,9 @@ gram = do xs <- many lr; nl; pure xs
     lr = alts [ do lit '<'; pure L
               , do lit '>'; pure R]
 
-part1 :: Setup -> IO Int
+part1 :: Setup -> Int
 part1 q = do
-  print "part1"
-  let ss = sim q
-  let pr (i,s) = print (i, height s, s)
-  mapM_ pr (zip [0::Int ..] ss)
-  pure 99
+  height (last (sim q))
 
 data State = State
   { solid :: Set Pos
@@ -39,19 +32,7 @@ data State = State
   , upcoming :: [Set Pos]
   }
 
-instance Show State where
-  show State{falling,solid} =
-    show (Set.size solid, Set.toList falling)
-
 type Pos = (Int,Int)
-data LR = L | R deriving Show
-
-sim :: Setup -> [State]
-sim q = loop (initState q)
-  where
-    loop s = s : loop2 (blow s)
-    loop2 s = s : loop (fall s)
-
 
 initState :: [LR] -> State
 initState wind = do
@@ -59,11 +40,10 @@ initState wind = do
   let windy = wind ++ windy
   State
     { wind = windy
-    , upcoming = tail (take 2023 upcoming)
+    , upcoming = tail (take 2022 upcoming)
     , falling = Set.map (locate (3,4)) (head upcoming)
     , solid = Set.empty
     }
-
 
 shapes :: [Set Pos]
 shapes = map Set.fromList
@@ -74,23 +54,11 @@ shapes = map Set.fromList
   , [ (0,0), (1,0), (0,1), (1,1) ]
   ]
 
-fall :: State -> State
-fall s@State{falling} = do
-  let s' = s { falling = Set.map moveDown falling }
-  if impossible s' then freeze s else s'
-
-freeze :: State -> State
-freeze s@State{solid,falling,upcoming} = do
-  let s' = s { solid = solid `Set.union` falling }
-  let maxY = height s'
-  s'
-    { falling = Set.map (locate (3,maxY+4)) (head upcoming)
-    , upcoming = tail upcoming
-    }
-
-height :: State -> Int
-height State{solid} =
-  maximum [ y | (_,y) <- (0,0) : Set.toList solid ]
+sim :: Setup -> [State]
+sim q = loop (initState q)
+  where
+    loop s = s : loop2 (blow s)
+    loop2 s = s : case fall s of Right s -> loop s; Left s -> [s]
 
 blow :: State -> State
 blow s@State{falling,wind} = do
@@ -98,11 +66,30 @@ blow s@State{falling,wind} = do
   let s' = s { falling = Set.map move falling }
   (if impossible s' then s else s') { wind = tail wind }
 
+fall :: State -> Either State State
+fall s@State{falling} = do
+  let s' = s { falling = Set.map moveDown falling }
+  if impossible s' then freeze s else Right s'
+
+freeze :: State -> Either State State
+freeze s@State{solid,falling,upcoming} = do
+  let s' = s { solid = solid `Set.union` falling }
+  case upcoming of
+    [] -> Left s'
+    u:upcoming -> do
+      Right $ s'
+        { falling = Set.map (locate (3, height s' + 4)) u
+        , upcoming
+        }
+
+height :: State -> Int
+height State{solid} =
+  maximum [ y | (_,y) <- (0,0) : Set.toList solid ]
+
 impossible :: State -> Bool
 impossible State{falling,solid} =
   any (not . inBounds) falling
-  ||
-  not (Set.null (falling `Set.intersection` solid))
+  || not (Set.null (falling `Set.intersection` solid))
 
 inBounds :: Pos -> Bool
 inBounds (x,y) = x>=1 && x<=7 && y>=1
